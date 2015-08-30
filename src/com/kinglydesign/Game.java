769d332738531f68129d9b3e6bf10f2f3944995f
Kody Simpson <narwhal0616@gmail.com>
2015-08-30 17:11:47 -0500

    Re-Setup

---
+++ src/com/kinglydesign/Game.java
@@ -0,0 +1,11 @@
+package com.kinglydesign;
+
+/**
+ * Created by kody on 8/30/2015.
+ */
+public class Game {
+
+    public static int width = 1920;
+    public static int height = 1080;
+    public static int scale = 3;
+}
